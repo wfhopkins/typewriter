e508214efd360b87efcbfdf25a2a1225111f90cc
wfhopkins <will.hop@gmail.com>
2023-06-06 20:14:37 -0700

    updated typewriter to correctly begin new line after typing input

--- typewriter.js
+++ typewriter.js
@@ -2,19 +2,22 @@ const sentence = "hello there from lighthouse labs";
 const typewriter = function(sentence) {
   let delay = 50;
   for (const char of sentence) {
-    if (char === char[sentence.length - 1]){
-      process.stdout.write("\n");
-    } else {  
+    console.log("char: ", char);
+    console.log("delay: ", delay);
+
     setTimeout(() => {
       process.stdout.write(char)
     }, delay);
     delay += 50;
-    }
   }
+  setTimeout(() => {
+    // console.log("itLives!");
+    process.stdout.write("\n");
+  }, delay);
 };
 typewriter(sentence);
 
-
+//delay * (sentence.length - 1)
 // if (char === char[sentence.length -1 && char !== " "]) {
 //   process.stdout.write("\n");
 
